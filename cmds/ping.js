44ac095e413b0851139c4bf4c724c6383ae8fca1
Danon4ik <fadescode@gmail.com>
2020-11-26 03:36:46 +0500

    Removed asynchronous method

--- cmds/ping.js
+++ cmds/ping.js
@@ -1,10 +1,10 @@
 module.exports = {
 	name: 'ping',
 	description: 'Тестовая команда, которая проверяет задержку.',
-	async execute(message) {
-		await message.channel.send("Проверяем задержку...").then(async m =>{
-            var ping = await m.createdTimestamp - message.createdTimestamp;
-            await m.edit(`Задержка между сообщениями - ${ping} мс.`);
+	execute(message) {
+		message.channel.send('Проверяем задержку...').then(m => {
+            var ping = m.createdTimestamp - message.createdTimestamp;
+            m.edit(`Задержка между сообщениями - ${ping} мс.`);
         });
 	},
 };
