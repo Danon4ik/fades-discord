b14207d6289a0edb541c1105ded38dbf7c458dc7
Danon4ik <fadescode@gmail.com>
2020-12-04 02:43:59 +0500

    Added initusers command Just initialization users to MySQL database.

---
+++ cmds/admin/initusers.js
@@ -0,0 +1,25 @@
+const { initUsers } = require('../../utils/mysql_queries');
+
+module.exports = {
+  name: 'initusers',
+  description: 'Добавление всех участников на сервере в базу данных.',
+  execute(message) {
+    const members = message.guild.members;
+    const author = members.cache.get(message.author.id);
+
+    if (!author.hasPermission(['ADMINISTRATOR'])) {
+      message.reply('У вас нет прав на использование этой команды.')
+      return;
+    }
+
+    let membersID = [];
+    members.cache.map(member => {
+      if (member.user.bot) return;
+      membersID.push(member.id)
+    });
+    
+    initUsers(membersID);
+
+    message.channel.send('Все участники были успешно добавлены в базу данных!').catch(console.error);
+  }
+};
