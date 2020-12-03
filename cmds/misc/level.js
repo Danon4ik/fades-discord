38da55d76409f23aec376780b32d4a2afd216c53
Danon4ik <fadescode@gmail.com>
2020-12-04 02:48:36 +0500

    Added level command For check your or someone's level and experience

---
+++ cmds/misc/level.js
@@ -0,0 +1,26 @@
+const { MessageEmbed } = require('discord.js');
+const { getUserLevel } = require('../../utils/mysql_queries');
+
+module.exports = {
+  name: 'level',
+  description: 'Выдает информацию об уровне пользователя.',
+  execute(message) {
+    const author = message.author;
+    const mention = message.mentions.users.first();
+
+    const user = mention ? mention : author;
+    getUserLevel(user.id, data => {
+      const desc = `Уровень: ${data.lvl}\n`
+        + `Опыт: ${data.user_xp} / ${data.max_xp} xp\n`
+        + `До следующего уровня: ${data.max_xp - data.user_xp} xp`;
+
+      const embed = new MessageEmbed()
+        .setTitle(`Информация об уровне ${user.tag}`)
+        .setColor('#85107F')
+        .setDescription(desc)
+        .setThumbnail(user.displayAvatarURL({ dynamic: true, size: 256 }));
+
+      message.reply(embed).catch(console.error);
+    });
+  }
+};
