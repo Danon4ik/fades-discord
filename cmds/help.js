6352e1368d7adde59d41e9882f5db0fef35423a2
Danon4ik <fadescode@gmail.com>
2020-12-04 17:19:36 +0500

    Updated help command - Changed help embed for categories - Added help for specific command

--- cmds/help.js
+++ cmds/help.js
@@ -4,21 +4,47 @@ module.exports = {
   name: 'help',
   description: 'Отображает все команды с их описаниями.',
   category: 'Разное',
-  example: 'help - отображает все команды\nhelp `наименование команды` - отображает пример использования команды',
-  execute(message) {
+  example: 'help - отображает все команды\nhelp \'наименование команды\' - отображает пример использования команды',
+  execute(message, args) {
     const commands = message.client.commands.array();
+    const arg = args[0] || false;
 
-    let description = '';
-    for (let i = 0; i < commands.length; i++) {
-      const cmd = commands[i];
-      description = description.concat([`**${cmd.name}** - ${cmd.description}\n`]);
+    if (!arg) {
+      let fields = {};
+      const embed = new MessageEmbed()
+        .setTitle('Список всех команд')
+        .setColor('#85107F')
+        .setDescription('Ниже приведен список всех команд с их описаниями.');
+
+      for (let i = 0; i < commands.length; i++) {
+        const cmd = commands[i];
+
+        if (!fields[cmd.category]) fields[cmd.category] = '';
+        fields[cmd.category] = fields[cmd.category].concat([`\`${cmd.name}\` - ${cmd.description}\n`]);
+      }
+      
+      Object.entries(fields).forEach(([key, value]) => embed.addField(key, value));
+      message.channel.send(embed).catch(console.error);
+    } else {
+      let command;
+      for (let i = 0; i < commands.length; i++) {
+        const cmd = commands[i];
+        if (cmd.name === arg) {
+          command = cmd;
+          break;
+        }
+      }
+
+      if (!command) {
+        message.reply('Такой команды не существует!');
+      }
+
+      const embed = new MessageEmbed()
+        .setTitle(`Команда - ${command.name}`)
+        .setColor('#85107F')
+        .setDescription(`Описание: ${command.description}\nПример: \`${command.example}\``);
+
+      message.channel.send(embed).catch(console.error);
     }
-
-    const embed = new MessageEmbed()
-      .setTitle('Список всех команд')
-      .setColor('#85107F')
-      .setDescription(description);
-
-    message.channel.send(embed).catch(console.error);
   }
 };
